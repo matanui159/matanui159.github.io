a6aabc91f03190428b9b688f0274389c4dc22107
Joshua Minter <josh@minteronline.com>
2018-09-06 09:24:09 +1000

    Working on new design

--- script.js
+++ script.js
@@ -21,25 +21,18 @@ function resize() {
 window.addEventListener("resize", resize)
 resize()
 
-var mouse_x = 0
-var mouse_y = 0
+var bit_prev = 0
 window.addEventListener("mousemove", function(event) {
 	var bits = $("bit", true)
-	for (var i = 0; i < bits.length; ++i) {
-		var rect = bits[i].getBoundingClientRect()
-		if (mouse_y < rect.top || mouse_y > rect.bottom) {
-			if (event.clientY >= rect.top && event.clientY <= rect.bottom) {
-				var bit = bits[i].innerHTML.trim()
-				if (bit === "0") {
-					bit = "1"
-				} else {
-					bit = "0"
-				}
-				bits[i].innerHTML = bit
-			}
+	var index = Math.floor(event.clientY / bits[0].offsetHeight)
+	if (index !== bit_prev) {
+		var bit = bits[index].innerHTML
+		if (bit === "0") {
+			bit = "1"
+		} else {
+			bit = "0"
 		}
+		bits[index].innerHTML = bit
+		bit_prev = index
 	}
-
-	mouse_x = event.clientX
-	mouse_y = event.clientY
 })

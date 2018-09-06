0d81617d0d412c5308a9cc3791c4adbce611eeb3
Joshua Minter <josh@minteronline.com>
2018-09-06 22:57:36 +1000

    Moved to jekyll

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
-function $(name, list) {
-	var result = document.getElementsByClassName(name)
-	if (list) {
-		return result
+function $(query) {
+	if (query.includes('+')) {
+		var half = query.split('+')
+		var elem = document.createElement(half[1])
+		$(half[0]).appendChild(elem)
+		return elem
+	} else if (query.charAt(0) === '@') {
+		return document.querySelectorAll(query.substring(1))
 	} else {
-		return result[0]
+		return document.querySelector(query)
 	}
 }
 
@@ -12,20 +16,19 @@ function $(name, list) {
  */
 var bit_count = 1
 window.addEventListener("resize", function() {
-	var count = window.innerHeight / $("bit").offsetHeight
+	var count = window.innerHeight / $(".bit").offsetHeight
 	for (; bit_count < count; ++bit_count) {
 		console.log()
-		var bit = document.createElement("div")
+		var bit = $('.binary+div')
 		bit.className = "bit"
 		bit.innerHTML = Math.floor(Math.random() * 2)
-		$("binary").appendChild(bit)
 	}
 })
 window.dispatchEvent(new Event("resize"))
 
 var bit_prev = 0
 window.addEventListener("mousemove", function(event) {
-	var bits = $("bit", true)
+	var bits = $("@.bit")
 	var index = Math.floor(event.clientY / bits[0].offsetHeight)
 	if (index !== bit_prev) {
 		var bit = bits[index].innerHTML.trim()
@@ -42,21 +45,19 @@ window.addEventListener("mousemove", function(event) {
 /*
  * Git repo
  */
-function gitcallback(response) {
-	console.log(response)
+function git_callback(response) {
 	var data = response.data
 	for (var i = 0; i < data.length; ++i) {
-		if (data[i].type === "PushEvent") {
+		if ("repo" in data[i]) {
 			var name = data[i].repo.name
-			var gitlink = $("gitlink")
-			gitlink.innerHTML = name
-			gitlink.href = "https://github.com/" + name
-			$("footer").style.display = "block"
+			var link = $(".git a")
+			link.innerHTML = name
+			link.href = "https://github.com/" + name
+			$(".git").style.display = "block"
 			break
 		}
 	}
 }
 
-var script = document.createElement("script")
-script.src = "https://api.github.com/users/matanui159/events?callback=gitcallback"
-document.body.appendChild(script)
+var script = $('body+script')
+script.src = "https://api.github.com/users/matanui159/events?callback=git_callback"
